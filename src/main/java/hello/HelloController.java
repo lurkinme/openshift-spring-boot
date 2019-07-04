b9bbed94f0089e90b139542ffe773f717a8a4858
Yury Smuraha <yury.smuraha@ihg.com>
2019-07-04 14:36:56 +0300

    Added /number route

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +11,11 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/number")
+    public String number(@RequestParam String value) {
+        return "Value: " + value;
     }
 
 }
